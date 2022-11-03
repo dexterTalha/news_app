14d44a3f464e7d9b09fd4569edba6d75d2052353
dextertalha <talhaviet22@gmail.com>
2022-11-03 21:34:47 +0530

    category selection

--- lib/screens/catergory_screen.dart
+++ lib/screens/catergory_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/components/primary_button_component.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({Key? key}) : super(key: key);
@@ -15,12 +16,19 @@ class _CategoryScreenState extends State<CategoryScreen> {
     "Crypto News",
   ];
 
+  List<String> selectedCat = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "Select Your Category",
+          style: TextStyle(color: Colors.black),
+        ),
         leading: IconButton(
           onPressed: () {},
           icon: const Icon(Icons.close_rounded, color: Colors.black),
@@ -35,6 +43,15 @@ class _CategoryScreenState extends State<CategoryScreen> {
           ),
         ],
       ),
+      bottomNavigationBar: Container(
+        height: AppBar().preferredSize.height,
+        width: double.maxFinite,
+        padding: const EdgeInsets.all(4),
+        child: PrimaryButtonComponent(
+          primaryText: "Continue",
+          onPrimaryClick: selectedCat.isEmpty ? null : () {},
+        ),
+      ),
       body: Container(
         width: double.maxFinite,
         height: double.maxFinite,
@@ -47,10 +64,35 @@ class _CategoryScreenState extends State<CategoryScreen> {
             crossAxisSpacing: 10,
           ),
           itemBuilder: (_, index) {
-            return Container(
-              color: Colors.red,
-              child: Center(
-                child: Text(newsType[index]),
+            String textValue = newsType[index];
+            bool isSelected = selectedCat.contains(textValue);
+            return GestureDetector(
+              onTap: () {
+                String value = newsType[index];
+                selectedCat.contains(value) ? selectedCat.remove(value) : selectedCat.add(value);
+                setState(() {});
+              },
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: isSelected ? Colors.blueAccent : Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      blurRadius: 4,
+                      spreadRadius: 1,
+                    )
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    textValue,
+                    style: TextStyle(
+                      color: isSelected ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
               ),
             );
           },
